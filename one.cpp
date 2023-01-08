d8b1f76e19bd6cf10310a9563d8ff0c7052ebd1d
2019331051 <shamim51@student.sust.edu>
2023-01-09 02:26:47 +0600

    changing

---
+++ one.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string ss;
+    int n;
+    cin>>n;
+    for(int i=0;i<n+100;i++)cout<<i<<endl;
+    cin>>ss;
+    cout<<"shamim"<<endl;
+}
